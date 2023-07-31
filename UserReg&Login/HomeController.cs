a4fee3b5de7f7ba7c0d529bb24eefb0909c37e7b
Weijie Li <64340331+137315198@users.noreply.github.com>
2023-07-31 19:28:44 +0800

    user registration and authentication
    HomeController (or equivalent default controller) implementation with user registration and authentication

--- UserReg&Login/HomeController.cs
+++ UserReg&Login/HomeController.cs
@@ -4,8 +4,9 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
-namespace FIT5032_MyViewModel.Controllers
+namespace FIT5032_MyIdentity.Controllers
 {
+    [RequireHttps]
     public class HomeController : Controller
     {
         public ActionResult Index()
